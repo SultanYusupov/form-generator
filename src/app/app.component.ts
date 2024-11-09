9340a3d7dca58dbb9327d6999d8144e2d5468db3
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-09 13:19:26 +0400

    Новый компонент с инпутом

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TestInputComponent} from './components/test-input/test-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, TestInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/test-input/test-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-input.component.html',
+  styleUrl: './test-input.component.scss'
+})
+export class TestInputComponent {
+
+}
