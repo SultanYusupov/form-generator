9b1c59d73b22bbc5da97218b23bbe0292fa5381b
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-10 23:23:05 +0400

    Новые компоненты

---
+++ src/app/components/test-select/test-select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-select.component.html',
+  styleUrl: './test-select.component.css'
+})
+export class TestSelectComponent {
+
+}
