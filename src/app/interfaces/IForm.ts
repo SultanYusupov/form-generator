5f57116539bca47edb3e9cc717f196a137c4d65d
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-11 23:37:29 +0400

    Другое представление компонентов, без вложенного цикла

--- src/app/interfaces/IForm.ts
+++ src/app/interfaces/IForm.ts
@@ -1,6 +1,14 @@
 import {IInput} from './IInput';
 
 export interface IForm {
+  // type: string,
+  // formList: IInput[]
+  id: string,
   type: string,
-  formList: IInput[]
+  labelName: string,
+  inputName: string,
+  required: boolean,
+  options?: string[],
+  min?: number,
+  max?: number
 }
