4e1ef83c5ddfee55a81d3de62ba13017eb637da0
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-12 15:34:35 +0400

    Убрал лишние тэги в test-input

--- src/app/components/test-input/test-input.component.ts
+++ src/app/components/test-input/test-input.component.ts
@@ -1,15 +1,13 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {IInput} from '../../interfaces/IInput';
 import {ControlContainer, FormArray, FormGroup, FormGroupName, ReactiveFormsModule} from '@angular/forms';
 import {IForm} from '../../interfaces/IForm';
-import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'test-input',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    NgForOf
   ],
   templateUrl: './test-input.component.html',
   styleUrl: './test-input.component.scss',
