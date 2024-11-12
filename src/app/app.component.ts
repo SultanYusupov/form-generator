125a6e8d79f38239a7c6adde1e973872204f3b04
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-12 15:30:13 +0400

    Пробую сделать динамическое создание формы. Пока только для test-input.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {TestInputComponent} from './components/test-input/test-input.component';
 import {BackendService} from './services/backend.service';
 import {IForm} from './interfaces/IForm';
@@ -19,7 +19,6 @@ import {TestCheckboxComponent} from './components/test-checkbox/test-checkbox.co
 export class AppComponent implements OnInit{
   title = 'form-generator';
   forms: IForm[] = [];
-  iterator: any;
   bs = inject(BackendService);
   fb = inject(FormBuilder);
   testForm!: FormGroup; // массив forms может быть пустым, поэтому знак вопроса
@@ -31,12 +30,25 @@ export class AppComponent implements OnInit{
   getForms() {
     this.bs.getFormList().subscribe(data => {
       this.forms = data;
-      // this.iterator = this.forms[0]['formList'][Symbol.iterator]();
+      // let control: FormControl<string | null> | FormArray<FormControl<string | null>>;
       this.forms.forEach(el => {
-        const control = this.fb.control('', el.required ? Validators.required : null);
-        this.testForm.addControl(el.inputName, control);
+        if (el.multiply) {
+          // control = this.fb.array(el.options as string[], el.required ? Validators.required : null);
+          const formArray: FormArray<FormControl<any>> = this.fb.array([]);
+          this.testForm.addControl(el.inputName, formArray);
+          const control: FormArray = this.testForm.get(el.inputName) as FormArray;
+          control.push(this.fb.control(''));
+        }
+        else {
+          const control: FormControl<string | null> = this.fb.control('', el.required ? Validators.required : null);
+          this.testForm.addControl(el.inputName, control);
+        }
       })
 
     });
+  }
+
+  createForm(form: FormGroup, forms: IForm[]) {
+
   }
 }
--- src/app/components/test-input/test-input.component.ts
+++ src/app/components/test-input/test-input.component.ts
@@ -1,18 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {IInput} from '../../interfaces/IInput';
-import {ControlContainer, FormGroup, FormGroupName, ReactiveFormsModule} from '@angular/forms';
+import {ControlContainer, FormArray, FormGroup, FormGroupName, ReactiveFormsModule} from '@angular/forms';
+import {IForm} from '../../interfaces/IForm';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'test-input',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgForOf
   ],
   templateUrl: './test-input.component.html',
   styleUrl: './test-input.component.scss',
-  viewProviders: [{ provide: ControlContainer, useExisting: FormGroupName }]
+  // viewProviders: [{ provide: ControlContainer, useExisting: FormGroupName }]
 })
 export class TestInputComponent {
-  @Input() inputData!: IInput;
+  @Input() inputData!: IForm;
   @Input() fControl!: FormGroup;
+
+  getControls() {
+    return (this.fControl.get(this.inputData.inputName) as FormArray).controls;
+  }
 }
