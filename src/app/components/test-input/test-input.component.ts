c3c0ecf08adc9eabddc08e9e9751ad2c03e375a0
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-13 16:10:37 +0400

    Переместил form-container, кнопку "Добавить ещё" и метод addFormControl() в компонент

--- src/app/components/test-input/test-input.component.ts
+++ src/app/components/test-input/test-input.component.ts
@@ -1,13 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {IInput} from '../../interfaces/IInput';
-import {ControlContainer, FormArray, FormGroup, FormGroupName, ReactiveFormsModule} from '@angular/forms';
+import {
+  ControlContainer,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupName,
+  ReactiveFormsModule
+} from '@angular/forms';
 import {IForm} from '../../interfaces/IForm';
+import {AddButtonComponent} from '../add-button/add-button.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'test-input',
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    AddButtonComponent,
+    NgIf,
   ],
   templateUrl: './test-input.component.html',
   styleUrl: './test-input.component.scss',
@@ -19,5 +31,9 @@ export class TestInputComponent {
 
   getControls() {
     return (this.fControl.get(this.inputData.inputName) as FormArray).controls;
+  }
+
+  addFormControl(inputName: string) {
+    (this.fControl.get(inputName) as FormArray).push(new FormControl())
   }
 }
