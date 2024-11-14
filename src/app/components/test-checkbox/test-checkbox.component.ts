0bd834160ce255d9f65e78f6777016e31b820ef9
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-14 22:30:39 +0400

    Формы для чекбоксов

--- src/app/components/test-checkbox/test-checkbox.component.ts
+++ src/app/components/test-checkbox/test-checkbox.component.ts
@@ -1,14 +1,37 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {IInput} from '../../interfaces/IInput';
 import {IForm} from '../../interfaces/IForm';
+import {FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'test-checkbox',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './test-checkbox.component.html',
   styleUrl: './test-checkbox.component.css'
 })
 export class TestCheckboxComponent {
   @Input() inputData!: IForm;
+  @Input() fControl!: FormGroup;
+  @Output() add: EventEmitter<string> = new EventEmitter();
+  @Output() remove: EventEmitter<{inputName: string, index: number}> = new EventEmitter();
+
+  ngOnInit() {
+    this.fControl.addControl(this.inputData.inputName, new FormArray([]));
+  }
+
+  choseOption($event:Event, index: number) {
+    const checkStatus = ($event.target as HTMLInputElement).checked;
+    if (checkStatus) {
+      const inputValue = ($event.target as HTMLInputElement).value;
+      this.add.emit(inputValue)
+    } else {
+      this.remove.emit({inputName: this.inputData.inputName, index: index});
+    }
+  }
 }
