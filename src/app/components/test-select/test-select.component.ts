737cddf383e9d202f373c9e81a5da247ba5e9c78
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-14 17:50:58 +0400

    Начинаю делать кастомный селект

--- src/app/components/test-select/test-select.component.ts
+++ src/app/components/test-select/test-select.component.ts
@@ -1,14 +1,22 @@
 import {Component, Input} from '@angular/core';
 import {IInput} from '../../interfaces/IInput';
 import {IForm} from '../../interfaces/IForm';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'test-select',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './test-select.component.html',
   styleUrl: './test-select.component.css'
 })
 export class TestSelectComponent {
   @Input() inputData!: IForm;
+  opened:boolean = false;
+
+  openSelect() {
+    this.opened = !this.opened;
+  }
 }
