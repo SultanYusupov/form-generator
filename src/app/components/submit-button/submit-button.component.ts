f693616e7fcdb79cc1ece71f4240ae8cddd42c9f
Sultan <s.yusupov.prog@solo-it.ru>
2024-11-15 20:11:44 +0400

    Компонент кнопки отправки формы

---
+++ src/app/components/submit-button/submit-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'submit',
+  standalone: true,
+  imports: [],
+  templateUrl: './submit-button.component.html',
+  styleUrl: './submit-button.component.scss'
+})
+export class SubmitButtonComponent {
+
+}
